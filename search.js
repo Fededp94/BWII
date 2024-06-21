41fc044e5470b67c27fff7f95421755e7673390b
Federico Di Presa <federicodipresa94@gmail.com>
2024-06-21 15:06:35 +0200

    upgrade

--- search.js
+++ search.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   const searchInput = document.querySelector(".search-container input");
   const searchContainer = document.querySelector(".search-container");
+
+  const resultContainer = document.createElement("div");
+  resultContainer.className = "result-container";
   searchContainer.appendChild(resultContainer);
 
   searchInput.addEventListener("keyup", (event) => {
@@ -18,7 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
       method: "GET",
       headers: {
         "X-RapidAPI-Key": "8069aeef3cmshb149ad9eb9fd6dfp16871cjsneeeaf74616ad",
-        "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+        "X-RapidAPI-Host": "songstats.p.rapidapi.com",
       },
     };
 
@@ -64,74 +67,133 @@ document.addEventListener("DOMContentLoaded", () => {
   };
 
   const playTrack = (trackId) => {
-    DZ.init({
-      appId: "YOUR_DEEZER_APP_ID",
-      channelUrl: "YOUR_CHANNEL_URL",
-      player: {
-        container: "player",
-        playlist: true,
-        width: 300,
-        height: 300,
-        format: "square",
-        onload: function () {
-          DZ.player.playTracks([trackId]);
-        },
-      },
-    });
+    console.log(`Playing track with ID: ${trackId}`);
   };
 });
+document.addEventListener("DOMContentLoaded", () => {
+  const songItems = document.querySelectorAll(".song-item");
+  const titleSong = document.querySelector(".title-song");
+  const artistSong = document.querySelector(".artist-song");
+  const durationSong = document.querySelector(".duration-song");
+  const currentDuration = document.querySelector(".current-duration");
+  const playBtn = document.querySelector(".play-btn");
+  const nextBtn = document.querySelector(".next-btn");
+  const previousBtn = document.querySelector(".previous-btn");
+  const progressBar = document.querySelector(".progress-bar");
+  // const volumeBar = document.querySelector(".volume-bar");
+  // const volumeContainer = document.querySelector(".progress.mx-2");
 
-// playbar
+  let currentSongIndex = 0;
+  let intervalId;
+  let isPlaying = false;
+  // let volumeLevel = 0.3;
 
-let playing = false;
-let currentTime = 0;
-let duration = 180;
-let progressInterval;
+  function updatePlayerBar(index) {
+    const songItem = songItems[index];
+    const songTitle = songItem
+      .querySelector(".titolo-canzone")
+      .childNodes[0].nodeValue.trim();
+    const songArtist = songItem
+      .querySelector(".artista-canzone")
+      .innerText.trim();
+    const songDuration = songItem
+      .querySelector(".durata-canzone")
+      .innerText.trim();
 
-const playButton = document.querySelector(".play-btn");
-const progressBar = document.querySelector(".progress-bar");
-
-playButton.addEventListener("click", () => {
-  playing = !playing;
-
-  if (playing) {
-    playButton.classList.remove("bi-play-circle-fill");
-    playButton.classList.add("bi-pause-circle-fill");
-    startProgress();
-  } else {
-    playButton.classList.remove("bi-pause-circle-fill");
-    playButton.classList.add("bi-play-circle-fill");
-    clearInterval(progressInterval);
+    titleSong.innerText = songTitle;
+    artistSong.innerText = songArtist;
+    durationSong.innerText = songDuration;
+    currentDuration.innerText = "0:00";
+    progressBar.style.width = "0%";
+    clearInterval(intervalId);
+    isPlaying = false;
+    updatePlayButton();
   }
-});
 
-function startProgress() {
-  progressInterval = setInterval(() => {
-    if (currentTime < duration) {
-      currentTime++;
-      updateProgressBar();
+  function startPlayback() {
+    clearInterval(intervalId);
+    let [minutes, seconds] = currentDuration.innerText.split(":").map(Number);
+    let currentSeconds = minutes * 60 + seconds;
+    let [totalMinutes, totalSeconds] = durationSong.innerText
+      .split(":")
+      .map(Number);
+    const totalDurationSeconds = totalMinutes * 60 + totalSeconds;
+
+    intervalId = setInterval(() => {
+      if (currentSeconds >= totalDurationSeconds) {
+        clearInterval(intervalId);
+      } else {
+        currentSeconds++;
+        const curMinutes = Math.floor(currentSeconds / 60);
+        const curSeconds = currentSeconds % 60;
+        currentDuration.innerText = `${curMinutes}:${
+          curSeconds < 10 ? "0" : ""
+        }${curSeconds}`;
+
+        const progressPercentage =
+          (currentSeconds / totalDurationSeconds) * 100;
+        progressBar.style.width = `${progressPercentage}%`;
+      }
+    }, 1000);
+  }
+
+  function pausePlayback() {
+    clearInterval(intervalId);
+  }
+
+  function updatePlayButton() {
+    if (isPlaying) {
+      playBtn.classList.remove("bi-play-circle-fill");
+      playBtn.classList.add("bi-pause-circle-fill");
     } else {
-      clearInterval(progressInterval);
-      playing = false;
-      playButton.classList.remove("bi-pause-circle-fill");
-      playButton.classList.add("bi-play-circle-fill");
+      playBtn.classList.remove("bi-pause-circle-fill");
+      playBtn.classList.add("bi-play-circle-fill");
     }
-  }, 1000);
-}
+  }
 
-function updateProgressBar() {
-  const progressPercent = (currentTime / duration) * 100;
-  progressBar.style.width = progressPercent + "%";
-}
+  // function setVolume(level) {
+  //   volumeLevel = level;
+  //   volumeBar.style.width = `${volumeLevel * 100}%`;
+  // }
 
-// Pulsante skip avanti
-document.querySelector(".next-btn").addEventListener("click", () => {
-  currentTime = Math.min(currentTime + 30, duration);
-  updateProgressBar();
-});
+  playBtn.addEventListener("click", () => {
+    if (isPlaying) {
+      pausePlayback();
+    } else {
+      startPlayback();
+    }
+    isPlaying = !isPlaying;
+    updatePlayButton();
+  });
 
-// Pulsante skip indietro
-document.querySelector(".previous-btn").addEventListener("click", () => {
-  currentTime = Math.max(currentTime - 30, 0);
-  updateProgressBar();
+  nextBtn.addEventListener("click", () => {
+    currentSongIndex = (currentSongIndex + 1) % songItems.length;
+    updatePlayerBar(currentSongIndex);
+    if (isPlaying) startPlayback();
+  });
+
+  previousBtn.addEventListener("click", () => {
+    currentSongIndex =
+      (currentSongIndex - 1 + songItems.length) % songItems.length;
+    updatePlayerBar(currentSongIndex);
+    if (isPlaying) startPlayback();
+  });
+
+  songItems.forEach((item, index) => {
+    item.addEventListener("click", () => {
+      currentSongIndex = index;
+      updatePlayerBar(currentSongIndex);
+      if (isPlaying) startPlayback();
+    });
+  });
+
+  // volumeContainer.addEventListener("click", (e) => {
+  //   const rect = volumeContainer.getBoundingClientRect();
+  //   const offsetX = e.clientX - rect.left;
+  //   const volumePercentage = offsetX / rect.width;
+  //   setVolume(volumePercentage);
+  // });
+
+  updatePlayerBar(currentSongIndex);
+  // setVolume(volumeLevel);
 });
